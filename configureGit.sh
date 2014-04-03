7c705b12d6617a27b02d01eabbd69f632ef570c4
John Knowles <joknowle@calpoly.edu>
2014-04-03 12:56:59 -0700

    adding config and pledge file

---
+++ configureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "John Knowles"
+git config --global user.email "joknowle@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+
