17eb3cb7a5414991d4bcd9ac5517a698e8295cae
John Knowles <joknowle@calpoly.edu>
2014-05-24 05:33:32 -0700

    second push?? Maybe I fell asleep...

---
+++ SciPy/table.py
@@ -0,0 +1,5 @@
+object    term vel  init height  init vel  init angle
+
+golfball  44 m/s    0.0 m        66 m/s    45 degrees
+baseball  43 m/s    1.5 m        67 m/s    47 degrees
+pumpkin   107 m/s   1.5 m        66 m/s    39 degrees
